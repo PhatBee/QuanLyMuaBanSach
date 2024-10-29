69733c3ed50aabba471cd201cf60b413bbe8bfeb
PhatBee <vinhphatst1235@gmail.com>
2024-10-29 23:35:48 +0700

    updated

--- frmQuanLyTheLoai.cs
+++ frmQuanLyTheLoai.cs
@@ -16,5 +16,15 @@ namespace QuanLyMuaBanSach
         {
             InitializeComponent();
         }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmQuanLyTheLoai_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
