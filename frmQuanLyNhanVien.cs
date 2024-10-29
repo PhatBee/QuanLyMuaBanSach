8a60d2783f5574139162905ff25782f907375006
PhatBee <vinhphatst1235@gmail.com>
2024-10-29 18:10:39 +0700

    Complete QLNV beta (Chưa viết delete)

--- frmQuanLyNhanVien.cs
+++ frmQuanLyNhanVien.cs
@@ -79,5 +79,113 @@ namespace QuanLyMuaBanSach
             }
 
         }
+
+        private void btnSuaNV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string maNV = txtbxMaNV.Text;
+                string tenNV = txtbxTenNV.Text;
+                string gioiTinh = "";
+                if (rdobtnNam.Checked)
+                {
+                    gioiTinh = "Nam";
+                }
+                else if (rdobtnNu.Checked)
+                {
+                    gioiTinh = "Nữ";
+                }
+                else
+                {
+                    gioiTinh = "Khác";
+                }
+
+                DateTime ngaySinh = datipikrNgaySinh.Value;
+
+                string sdt = txtbxSDT.Text;
+                string diaChi = txtbxDiaChi.Text;
+                string unameMoi = txtbxUsernameMoi.Text;
+
+                mydb.openConection();
+
+                sqlCommand = new SqlCommand("proc_SuaThongTinNhanVien", mydb.getConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.AddWithValue("@maNV", maNV);
+                sqlCommand.Parameters.AddWithValue("@tenNV", tenNV);
+                sqlCommand.Parameters.AddWithValue("@gioiTinh", gioiTinh);
+                sqlCommand.Parameters.AddWithValue("@ngaySinh", ngaySinh);
+                sqlCommand.Parameters.AddWithValue("@SDT", sdt);
+                sqlCommand.Parameters.AddWithValue("@diaChi", diaChi);
+
+                sqlCommand.ExecuteNonQuery();
+
+                mydb.closeConection();
+
+                MessageBox.Show("Cập nhật thông tin nhân viên thành công!", "Cập nhật nhân viên", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cập nhật thông tin nhân viên thất bại. Mã lỗi: " + ex.Message, "Cập nhật nhân viên", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            frmQuanLyNhanVien_Load(sender, e);
+        }
+
+        private void frmQuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            dataNV.AutoGenerateColumns = true;
+            dataNV.DataSource = LayDanhSachNhanVien();
+
+            
+
+        }
+
+        public DataTable LayDanhSachNhanVien()
+        {
+            DataTable dt = new DataTable();
+            mydb.openConection();
+            sqlCommand = new SqlCommand("proc_LayDanhSachNhanVien", mydb.getConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand))
+            {
+                adapter.Fill(dt);
+            }
+            mydb.closeConection();
+            return dt;
+        }
+
+        private void dataNV_DoubleClick(object sender, EventArgs e)
+        { // MaNV, TenNV, GioiTinh, NgaySinh, SDT, DiaChi
+            try
+            {
+                txtbxMaNV.Text = dataNV.CurrentRow.Cells[0].Value.ToString();
+                txtbxTenNV.Text = dataNV.CurrentRow.Cells[1].Value.ToString();
+                if ((dataNV.CurrentRow.Cells[2].Value.ToString().Trim() == "Nam"))
+                {
+                    rdobtnNam.Checked = true;
+                }
+                if ((dataNV.CurrentRow.Cells[2].Value.ToString().Trim() == "Nữ"))
+                {
+                    rdobtnNu.Checked = true;
+                }
+                if ((dataNV.CurrentRow.Cells[2].Value.ToString().Trim() == "Khác"))
+                {
+                    rdobtnKhac.Checked = true;
+                }
+
+                datipikrNgaySinh.Value = (DateTime)dataNV.CurrentRow.Cells[3].Value;
+                txtbxSDT.Text = dataNV.CurrentRow.Cells[4].Value.ToString();
+                txtbxDiaChi.Text = dataNV.CurrentRow.Cells[5].Value.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
     }
 }
